a8e8c3b6367ec3dc00855c0a8cd4ca84619c1883
Andrew Socha <sochaa@smu.edu>
2016-01-20 15:44:36 -0600

    Remove Engine from branch
    Engine now has its own separate repository

--- Engine/Sound/cricket-1.4.2/doc/doxygen/customfile_8h.js
+++
@@ -1,5 +0,0 @@
-var customfile_8h =
-[
-    [ "CkCustomFileFunc", "customfile_8h.html#ga93ff8f5b0e2604308df66714893e1178", null ],
-    [ "CkSetCustomFileHandler", "customfile_8h.html#ga435ef2a65d93731b5b958f83eafe0da0", null ]
-];
--- Engine/Sound/cricket-1.4.2/doc/doxygen/search/variables_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['enablehardwaredecoding',['enableHardwareDecoding',['../struct___ck_config.html#a0acee4dafb84115956a92d129f9a2fe7',1,'_CkConfig']]],
-  ['enableoggvorbis',['enableOggVorbis',['../struct___ck_config.html#ae9a753d230560a3786ed520ed2f093a0',1,'_CkConfig']]]
-];
